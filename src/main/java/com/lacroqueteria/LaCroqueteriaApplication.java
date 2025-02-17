f2b60364c07837788b1cd018bd601d10760ebe71
Diego Camargo <dabrahamcc@gmail.com>
2025-02-17 13:09:09 -0600

    First commit

---
+++ src/main/java/com/lacroqueteria/LaCroqueteriaApplication.java
@@ -0,0 +1,13 @@
+package com.lacroqueteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaCroqueteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaCroqueteriaApplication.class, args);
+	}
+
+}
